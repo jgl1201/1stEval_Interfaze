de941ba88bc2af6ed5baf86ce9f137596bcc5478
jgl1201 <114144390+jgl1201@users.noreply.github.com>
2023-10-05 08:56:21 +0200

    Create 0.01_1stProof.js

---
+++ 0.01_1stProof.js
@@ -0,0 +1,11 @@
+// el punto y coma es para poner variables en la misma linea
+var firstColor = 200
+var secondColor = 150
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, firstColor, secondColor);
+}
